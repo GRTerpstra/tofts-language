3473b2f353f70a1cefea22899ee0d17542743ca2
GerwinTerpstra <gterpstra@quintor.nl>
2023-11-23 22:15:47 +0100

    Added package details and npmignore file

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@
 // You can use Babel for pure Typescript transpilation, but Jest will not type-check your tests as they are run. If you want that, you can use ts-jest instead, or just run the TypeScript compiler tsc separately (or as part of your build process).
 // ts-jest is a TypeScript preprocessor with source map support for Jest that lets you use Jest to test projects written in TypeScript.
 // To transpile TypeScript with ts-jest, there has to be a configuration file that will tell Jest to use a ts-jest preset.
+// Yo also need type definitions for Jest from the @types/jest package. Once installed, TypeScript will automatically recognize and use these type definitions when you import Jest functions and objects in your TypeScript files.
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
